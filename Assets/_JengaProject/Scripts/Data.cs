0e8a2b276dd61bc1279be8aeb746d6854fc46fb5
Gad2510 <gad2510@hotmail.com>
2023-05-26 08:38:05 -0600

    Data Functions
    Basic funtions of the data list to sort the info as require and filter de array with corresponing grade

--- Assets/_JengaProject/Scripts/Data.cs
+++ Assets/_JengaProject/Scripts/Data.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Linq;
 [System.Serializable]
 public class Data
 {
@@ -6,11 +6,16 @@ public class Data
 
     public Entry[] FilterByGrade(int grade)
     {
-        return lst_recordLst;
+        string filter = $"{grade}th Grade";
+        Entry[] result = null;
+        if (lst_recordLst.Any(x => x.grade == filter))
+            result = lst_recordLst.Where(x => x.grade == filter).ToArray();
+
+        return result;
     }
 
     public void SortList()
     {
-
+        lst_recordLst=lst_recordLst.OrderBy(x => x.domain).ThenBy(x=>x.cluster).ThenBy(x=>x.standardid).ToArray();
     }
 }
--- Assets/_JengaProject/Scripts/GameManager.cs
+++ Assets/_JengaProject/Scripts/GameManager.cs
@@ -37,5 +37,6 @@ public class GameManager : MonoBehaviour
     public void LoadData()
     {
         sd_GameData=HttpRequest.LoadData;
+        sd_GameData.SortList();
     }
 }
